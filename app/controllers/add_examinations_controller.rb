16e97b039f01f775e539ce35beee4d78dd009291
TowaYamashita <Confirm.Apps.Develop@gmail.com>
2020-04-01 16:28:03 +0900

    added before_action :authenticate_user!

--- app/controllers/add_examinations_controller.rb
+++ app/controllers/add_examinations_controller.rb
@@ -1,4 +1,5 @@
 class AddExaminationsController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
   before_action :set_add_examination, only: [:show, :edit, :update, :destroy]
 
   # GET /add_examinations
