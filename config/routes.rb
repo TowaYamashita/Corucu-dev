ad51eb7b514879a2264505e4818e18bac82d8922
TowaYamashita <Confirm.Apps.Develop@gmail.com>
2020-04-28 20:09:46 +0900

    reset controllers/users

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-  patch '/user/confirmation' => 'user/confirmations#update', :via => :patch, :as => :update_user_confirmation
 
   get 'contacts/new'
   get 'contacts/create'
